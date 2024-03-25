5f16fa47b4b8d4d3c1557733bd99df390e3202a6
AngeloGbiel <ngelog.iff@gmail.com>
2024-03-25 13:46:29 -0300

    dados ficticios + where

---
+++ where.sql
@@ -0,0 +1,7 @@
+select * from users;
+
+select * from users where id >= 50 AND id < 60;
+
+select * from users where id > 100 or first_name = "Hayfa" or first_name = "Angelo";
+
+select * from users where created_at < "2021-03-23";
