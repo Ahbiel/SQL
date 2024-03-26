df11cb75c3f9d46df8d64a20a826f56914884542
AngeloGbiel <ngelog.iff@gmail.com>
2024-03-26 15:08:48 -0300

    left e right join

--- join.sql
+++ join.sql
@@ -1,10 +1,23 @@
--- INNER JOIN
 SELECT * FROM profile;
 SELECT * FROM users;
 
+-- INNER JOIN
 SELECT * from users as u INNER JOIN profile as p ON u.id = p.user_id;
+
 SELECT u.id uid, p.id pid, u.last_name LastName 
 from users as u 
 INNER JOIN profile as p ON u.id = p.user_id
 WHERE u.id <> 112
 ORDER BY u.id desc;
+
+-- LEFT JOIN
+SELECT * from users as u INNER JOIN profile as p ON u.id = p.user_id;
+SELECT u.id uid, p.id pid, u.last_name LastName 
+from users as u 
+LEFT JOIN profile as p ON u.id = p.user_id
+
+-- RIGHT JOIN
+SELECT * from users as u INNER JOIN profile as p ON u.id = p.user_id;
+SELECT u.id uid, p.id pid, u.last_name LastName 
+from users as u 
+RIGHT JOIN profile as p ON u.id = p.user_id
