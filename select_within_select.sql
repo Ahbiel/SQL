231210ffad58401ab662894ce68909c4d0c93e0e
AngeloGbiel <ngelog.iff@gmail.com>
2024-03-27 14:22:57 -0300

    select dentro de select

---
+++ select_within_select.sql
@@ -0,0 +1,7 @@
+INSERT INTO roles (name) VALUES ("GET"),("PUT"),("POST"),("DELETE");
+
+SELECT id, (SELECT id FROM roles ORDER BY RAND() LIMIT 1) FROM users;
+
+INSERT INTO users_roles (user_id, role_id)
+SELECT id, (SELECT id FROM roles ORDER BY RAND() LIMIT 1)
+FROM users;
